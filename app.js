814bf2c184d908249f476d8710e210ff93d0231d
Deepali2 <hdgarg@gmail.com>
2019-05-02 20:30:54 -0700

    creates game using vanilla Javascript

---
+++ app.js
@@ -0,0 +1,80 @@
+const wordCount = 10
+     
+let guessCount = 4;       
+let password = '';
+
+const start = document.getElementById('start'); //that is the button
+
+const toggleClasses = (element, ...classNames) => classNames.forEach(name => element.classList.toggle(name));
+
+const shuffle = array => {
+  let arrCopy = array.slice();
+  for (let i = arrCopy.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]];
+  }
+  return arrCopy;
+}
+
+const getRandomValues = (array, numberOfVals=wordCount) => shuffle(array).slice(0, numberOfVals);
+
+const setGuessCount = newCount => {
+  guessCount = newCount;
+  document.getElementById('guesses-remaining').innerText = `Guesses remaining ${guessCount}.`;
+};
+
+const compareWords = (word1, word2) => {
+  if (word1.length !== word2.length) throw 'Words must have the same length';
+  let count = 0;
+  for (let i = 0; i < word1.length; i++) {
+    if (word1[i] === word2[i]) count++;
+  }
+  return count;
+};
+
+const updateGame = e => {
+  if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
+    //grab the guessed word, check it against password, update view
+    let guess = e.target.innerText;
+    let similarityScore = compareWords(guess, password);
+    e.target.classList.add('disabled');
+    e.target.innerText = `${guess}---> Matching Letters: ${similarityScore}`;
+    setGuessCount(guessCount - 1);
+    //check if the game is over
+    if (similarityScore === password.length) {
+      toggleClasses(document.getElementById('winner'), 'hide', 'show');
+      this.removeEventListener('click', updateGame);
+    } else if (guessCount === 0) {
+      toggleClasses(document.getElementById('loser'), 'hide', 'show');
+      this.removeEventListener('click', updateGame);
+    }
+  }
+};
+
+const startGame = () => {
+  //get random words and append them to the DOM
+  let wordList = document.getElementById('word-list');
+  //'words' variable is from words.js
+  let randomWords = getRandomValues(words);
+  randomWords.forEach(word => {
+    let li = document.createElement('li');
+    li.innerText = word;
+    wordList.appendChild(li);
+  });
+  //set a secret password and the guess count display
+  password = getRandomValues(randomWords, 1)[0];
+  setGuessCount(guessCount);  
+  //add update listener for clicking on a word
+  wordList.addEventListener('click', updateGame);
+};
+
+
+//starts the game
+start.addEventListener('click', () => {
+  toggleClasses(document.getElementById('start-screen'), 'hide', 'show');
+  toggleClasses(document.getElementById('game-screen'), 'hide', 'show');
+  startGame();
+});
+
+
+
